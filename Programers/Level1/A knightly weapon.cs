202efb38ec878b032484788322cdb7d042e586e8
qkrwoaks <qkrwoaks5327@naver.com>
2023-02-06 14:26:19 +0900

    Save Temp Programers

---
+++ Programers/Level1/A knightly weapon.cs
@@ -0,0 +1,46 @@
+﻿namespace Level1
+{
+    internal class A_knightly_weapon
+    {
+        public int Solution(int number, int limit, int power)
+        {
+            int count = 0;
+
+            for (int i = 1; i <= number; i++)
+            {
+                int sum = CountOfDivisor(i);
+
+                if (sum > limit)
+                {
+                    count += power;
+                }
+
+                else
+                {
+                    count += sum;
+                }
+            }
+
+            return count;
+        }
+        private int CountOfDivisor(int num)
+        {
+            int divisorCount = 0;
+
+            // 제곱근으로 계산
+            for (int i = 1; i * i <= num; i++)
+            {
+                if (i * i == num)
+                {
+                    divisorCount++;
+                }
+                else if (num % i == 0)
+                {
+                    divisorCount += 2;
+                }
+            }
+
+            return divisorCount;
+        }
+    }
+}
---
+++ Programers/Level1/Number mate.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Level1
+{
+    internal class Number_mate
+    {
+        public string Solution(string X, string Y)
+        {
+            string answer = "";
+
+            List<int> x = new List<int>();
+            List<int> y = new List<int>();
+
+            foreach (int item in X.ToCharArray()) x.Add(item);
+            foreach (int item in Y.ToCharArray()) y.Add(item);
+
+            x.Sort(); y.Sort();
+
+            foreach (int xitem in x)
+            {
+                foreach (int yitem in y)
+                {
+                    if (xitem == yitem)
+                    {
+                        answer += xitem;
+                        Console.WriteLine("xitem : " + xitem);
+                        Console.WriteLine("yitem : " + yitem);
+                    }
+                }
+            }
+            if (answer == string.Empty) answer = "-1";
+            return answer;
+        }
+    }
+    class Test
+    {
+        public static void Main(string[] args)
+        {
+            Number_mate Number_mate = new Number_mate();
+            Console.WriteLine(Number_mate.Solution("100", "203045"));
+        }
+    }
+}
