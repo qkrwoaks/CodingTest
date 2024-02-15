e305b35bce315b21735e5a99783edb83fbc5e1a2
박재만 <82390527+qkrwoaks@users.noreply.github.com>
2024-02-15 23:04:28 +0900

    0️⃣ | 합성수 찾기 + 풀이 과정

---
+++ Programers/Level0/합성수 찾기.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Level0
+{
+    internal class 합성수_찾기
+    {
+        public int solution(int n)
+        {
+            int answer = 0;
+            for (int i = 4; i <= n; i++)
+            {
+                for (int j = 2; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        answer++;
+                        break;
+                    }
+                }
+            }
+            return answer;
+        }
+    }
+}
