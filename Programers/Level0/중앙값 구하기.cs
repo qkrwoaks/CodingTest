6814980497842c1dab2f7ac3367a67d891fe82b6
박재만 <82390527+qkrwoaks@users.noreply.github.com>
2024-02-18 23:29:25 +0900

    0️⃣ | 중앙값 구하기 + 풀이 과정

---
+++ Programers/Level0/중앙값 구하기.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Level0
+{
+    internal class 중앙값_구하기
+    {
+        public int solution(int[] array)
+        {
+            List<int> list = new List<int>(array);
+            list.Sort();
+            return list[list.Count / 2];
+        }
+    }
+}
