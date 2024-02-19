0ef98e912e6402b1d16bf10e5437c9d4b182a475
박재만 <82390527+qkrwoaks@users.noreply.github.com>
2024-02-19 11:13:35 +0900

    1️⃣ | 자연수 뒤집어 배열로 만들기 + 풀이 과정

---
+++ Programers/Level1/자연수 뒤집어 배열로 만들기.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Level1
+{
+    internal class 자연수_뒤집어_배열로_만들기
+    {
+        public int[] solution(long n)
+        {
+            List<int> list = new List<int>();
+            int count = (int)Math.Ceiling(Math.Log10(n));
+            for (int i = 0; i < count; i++)
+            {
+                list.Add((int)(n % 10));
+                n /= 10;
+            }
+            return list.ToArray();
+        }
+    }
+}
