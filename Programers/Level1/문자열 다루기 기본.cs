ca65bf42227adcb40fc01d4c3af6a803fd658da4
박재만 <82390527+qkrwoaks@users.noreply.github.com>
2024-02-19 11:13:14 +0900

    1️⃣ | 문자열 다루기 기본+ 풀이 과정

---
+++ Programers/Level1/문자열 다루기 기본.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Level1
+{
+    internal class 문자열_다루기_기본
+    {
+        public bool solution(string s)
+        {
+            try
+            {
+                Int32.Parse(s);
+            }
+            catch
+            {
+                return false;
+            }
+            if (s.Length == 4 || s.Length == 6)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
