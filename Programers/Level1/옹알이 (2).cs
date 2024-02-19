991eb227bc74adc70b2ef66c4113f5f04520254d
박재만 <82390527+qkrwoaks@users.noreply.github.com>
2024-02-19 14:32:14 +0900

    1️⃣ | 옹알이 (2) + 풀이 과정

---
+++ Programers/Level1/옹알이 (2).cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Level1
+{
+    internal class 옹알이__2_
+    {
+        public int solution(string[] babbling)
+        {
+            int answer = 0;
+            int except;
+            string str;
+            string[] check = { "aya", "ye", "woo", "ma" };
+            foreach (var item in babbling)
+            {
+                except = -1;
+                str = item;
+                for (int i = 0; i < check.Length; i++)
+                {
+                    if (i == except)
+                    {
+                        continue;
+                    }
+                    if (str.Length < check[i].Length)
+                    {
+                        continue;
+                    }
+                    string sub = str.Substring(0, check[i].Length);
+                    if (sub != check[i])
+                    {
+                        continue;
+                    }
+                    str = str.Remove(0, check[i].Length);
+                    except = i;
+                    i = -1;
+                }
+                if (str == string.Empty) answer++;
+            }
+            return answer;
+        }
+    }
+}
