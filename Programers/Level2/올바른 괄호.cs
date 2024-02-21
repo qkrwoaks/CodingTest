2406178cb32e62d1949b7530c161cdc452786841
박재만 <82390527+qkrwoaks@users.noreply.github.com>
2024-02-21 15:47:41 +0900

    2️⃣ | 올바른 괄호 + 풀이 과정

---
+++ Programers/Level2/올바른 괄호.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Level2
+{
+    internal class 올바른_괄호
+    {
+        public bool solution(string s)
+        {
+            Stack<char> stack = new Stack<char>();
+            try
+            {
+                foreach (char item in s)
+                {
+                    if (item.Equals('('))
+                    {
+                        stack.Push('s');
+                    }
+                    else if (item.Equals(')'))
+                    {
+                        stack.Pop();
+                    }
+                }
+            }
+            catch
+            {
+                return false;
+            }
+            if (stack.Count == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
