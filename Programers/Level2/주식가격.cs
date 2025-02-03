d12537f18533d7b024a09a47a659c7fc8bf855b6
qkrwoaks <qkrwoaks5327@naver.com>
2025-02-03 23:09:23 +0900

    2️⃣ | 주식가격

---
+++ Programers/Level2/주식가격.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Level2
+{
+    public class Solution
+    {
+        public int[] solution(int[] prices)
+        {
+            int[] answer = new int[prices.Length];
+            for (int i = 0; i < prices.Length - 1; i++)
+            {
+                int count = 0;
+                for (int j = i + 1; j < prices.Length; j++)
+                {
+                    count++;
+                    if (prices[i] > prices[j])
+                    {
+                        break;
+                    }
+                }
+                answer[i] = count;
+            }
+            answer[prices.Length - 1] = 0;
+            return answer;
+        }
+    }
+
+    internal class 주식가격
+    {
+        public static void Main()
+        {
+            Solution solution = new Solution();
+            solution.solution(new int[] { 1, 2, 3, 2, 3 });
+        }
+    }
+}
